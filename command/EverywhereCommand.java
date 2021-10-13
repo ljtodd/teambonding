95a5ba7a679ff7392c1ebf28bd3f967e844b02de
TylerMadden <tyler.madden@outlook.com>
2021-10-13 12:54:39 -0400

    added comments

--- command/EverywhereCommand.java
+++ command/EverywhereCommand.java
@@ -1,8 +1,15 @@
 public class EverywhereCommand implements Command {
+    /**
+     * 
+     * @param cadence Ties object to new instance of Military Cadence
+     */
     public EverywhereCommand(MilitaryCadence cadence){
         
     }
 
+    /**
+     * Runs the singEverywhereIGo in MilitaryCadence
+     */
     public void execute(){
         MilitaryCadence.singEverywhereIGo();
     }
--- command/InArmyCommand.java
+++ command/InArmyCommand.java
@@ -1,8 +1,15 @@
 public class InArmyCommand implements Command {
+    /**
+     * 
+     * @param cadence Ties object to new instance of Military Cadence
+     */
     public InArmyCommand(MilitaryCadence cadence){
         
     }
 
+    /**
+     * Runs the singInArmy in MilitaryCadence
+     */
     public void execute(){
         MilitaryCadence.singInArmy();
     }
