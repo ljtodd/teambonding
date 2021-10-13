afc9db459892e63d6f55af8e823d1bef35c73e87
ljtodd <loringt@email.sc.edu>
2021-10-13 13:12:16 -0400

    adding comments

--- command/MilitaryCadence.java
+++ command/MilitaryCadence.java
@@ -15,23 +15,31 @@ public class MilitaryCadence {
 
     }
     /**
-     * 
+     * Takes idontknow.txt and splits up the lyrics into lines and reads it
      */
     public static void singIDontKnow() {
         ArrayList<String> iDontKnowLyrics = readFile("command/idontknow.txt");
         printCadence(iDontKnowLyrics);
     }
-
+    /**
+     * Takes everywhere.txt and splits up the lyrics into lines and reads it
+     */
     public static void singEverywhereIGo() {
         ArrayList<String> everywhereIGoLyrics = readFile("command/everywhere.txt");
         printCadence(everywhereIGoLyrics);
     }
-
+    /**
+     * Takes inarmy.txt and splits up the lyrics into lines and reads it 
+     */
     public static void singInArmy() {
         ArrayList<String> inArmyLyrics = readFile("command/inarmy.txt");
         printCadence(inArmyLyrics);
     }
-
+    /**
+     * This takes the file and creates an array list of lyrics
+     * @param fileName this is the name of the file that will be split into an array list of strings
+     * @return this will return the lyrics/lines of the song
+     */
     public static ArrayList<String> readFile(String fileName) {
         ArrayList<String> lyrics = new ArrayList<String>();
         try {
@@ -51,6 +59,10 @@ public class MilitaryCadence {
         }
         return lyrics;
     }
+    /**
+     * This prints out the lyrics with the pause included
+     * @param song this is the array list of the song itself
+     */
     public static void printCadence(ArrayList<String> song) {
         for(String line : song) {
             System.out.println(line);
